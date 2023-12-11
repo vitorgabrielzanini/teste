eaf836e633d4a3c37e6a71c49aa19ec2c00a49b1
vitorgabrielzanini <153538534+vitorgabrielzanini@users.noreply.github.com>
2023-12-11 14:01:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const input = document.querySelector('input')
 
-input.addEventListener('keypress', () => {
-    let inputLength = input.value.length
+input.addEventListener('input', () => {
+    let inputLength = input.value.length;
 
-    if (inputLength ===3 || inputLength ===7) {
-        input.value += '.'
-    }else if (inputLength === 11) {
-        input.value += '-'
+    if (inputLength === 3 || inputLength === 7) {
+        input.value += '.';
+    } else if (inputLength === 11) {
+        input.value += '-';
     }
-    
-})
+});
